4bae7986fa025cee74bf656cb956f45b23ebdc57
div1spawncamper <div1spawncamper@gmail.com>
2023-07-20 17:36:06 +0200

    administrate_algunas_lineas_comentadas

--- portfolio/vlakvlog/app/controllers/admin/application_controller.rb
+++ portfolio/vlakvlog/app/controllers/admin/application_controller.rb
@@ -1,11 +1,7 @@
-# All Administrate controllers inherit from this
-# `Administrate::ApplicationController`, making it the ideal place to put
-# authentication logic or other before_actions.
-#
-# If you want to add pagination or other controller-level concerns,
-# you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
+    #helper Admin::Engine.helpers
+
     before_action :authenticate_admin
 
     def authenticate_admin
