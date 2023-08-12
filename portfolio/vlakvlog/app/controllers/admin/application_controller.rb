ab30e01ce6d24f81c5dab851138678a864f7f4e9
div1spawncamper <div1spawncamper@gmail.com>
2023-08-12 16:33:51 +0200

    fix-del-dashboard-administrate

--- portfolio/vlakvlog/app/controllers/admin/application_controller.rb
+++ portfolio/vlakvlog/app/controllers/admin/application_controller.rb
@@ -5,8 +5,11 @@ module Admin
     before_action :authenticate_admin
 
     def authenticate_admin
-      redirect_to new_user_session_path unless current_user&.has_role?(:admin)
+      if !current_user&.has_role?(:admin) && !user_signed_in?
+        redirect_to new_user_session_path
+      end
     end
+
 
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
