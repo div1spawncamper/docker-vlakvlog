f8ef19bba893edf18f8ff3b6cb2cbafeabc7941d
div1spawncamper <div1spawncamper@gmail.com>
2023-08-12 12:44:45 +0200

    añado-fix-tras-la-pifia

--- portfolio/vlakvlog/db/schema.rb
+++ portfolio/vlakvlog/db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_19_014844) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
