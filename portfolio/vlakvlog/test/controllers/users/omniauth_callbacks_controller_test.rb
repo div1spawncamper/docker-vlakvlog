3c2bcbed22d6c60fab13ad1082f57ff9232bb919
Manuel González <ma.gonzalez@stuart.com>
2023-08-12 22:33:52 +0200

    add missing request envs

--- portfolio/vlakvlog/test/controllers/users/omniauth_callbacks_controller_test.rb
+++ portfolio/vlakvlog/test/controllers/users/omniauth_callbacks_controller_test.rb
@@ -2,6 +2,8 @@ require "test_helper"
 
 class Users::OmniauthCallbacksControllerTest < ActionController::TestCase
   test "debería crear o encontrar un usuario al autenticarse con GitHub" do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+
     auth_data = {
       provider: "github",
       uid: "123456",
@@ -10,15 +12,18 @@ class Users::OmniauthCallbacksControllerTest < ActionController::TestCase
         nickname: "testuser"
       }
     }
-
+    
     OmniAuth.config.add_mock(:github, auth_data)
-
+    @request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
+    
     post :github
-
+  
     user = User.find_by(provider: auth_data[:provider], uid: auth_data[:uid])
     assert_not_nil user
-
-    assert user_signed_in?
+    assert_equal user.email, auth_data[:info][:email]
+    assert_equal user.username, auth_data[:info][:nickname]
+    assert_equal user.provider, auth_data[:provider]
+    assert_equal user.uid, auth_data[:uid] 
     assert_redirected_to root_path
   end
 end
