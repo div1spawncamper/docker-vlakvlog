4b409fd888dd4437c4d849c813d608d2944969c4
Manuel González <ma.gonzalez@stuart.com>
2023-08-12 22:15:47 +0200

    add missing testing config
    - Devise controller helpers
    - Enable test mode for OmniAuth
    - Disable fixtures for now

--- portfolio/vlakvlog/test/test_helper.rb
+++ portfolio/vlakvlog/test/test_helper.rb
@@ -3,11 +3,13 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
+  include Devise::Test::ControllerHelpers
+  OmniAuth.config.test_mode = true
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  #fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
