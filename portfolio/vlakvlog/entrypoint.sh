e012f9e321546ea20ccea3a68e07c360ae805abc
div1spawncamper <div1spawncamper@gmail.com>
2023-09-03 06:32:42 +0200

    0.0.1

--- portfolio/vlakvlog/entrypoint.sh
+++ portfolio/vlakvlog/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # Espera a que el servicio de base de datos esté listo
 #while ! pg_isready -U postgres -h db -p 5432 >/dev/null 2>&1; do
@@ -6,14 +6,12 @@
 #done
 
 #ECHO
-#echo "Ejecutando db:migrate"
-#bundle exec rails db:migrate
-#ECHO
-#echo "Ejecutado db:migrate"
+echo "Ejecutando db:migrate"
+bundle exec rails db:migrate
 
 #ECHO
-#echo "Levantando rails server"
-#bundle exec rails server -b 0.0.0.0
-#ECHO
-#echo "Rails server levantado"
-echo "entrypoint vacío"
+echo "Levantando rails server"
+bundle exec rails server -b 0.0.0.0
+
+
+
